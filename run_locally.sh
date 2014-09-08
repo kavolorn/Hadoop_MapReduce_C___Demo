8948fcc7d4d03ae06afbaacd2bfa65b2ff343c07
Alexey Kornilov <kavolorn@gmail.com>
2014-09-08 23:07:43 +0300

    Small change in local runner has been made.

--- run_locally.sh
+++ run_locally.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 HADOOP_HOME=/opt/hadoop-2.3.0
 BINARY_HOME=/home/kavolorn/master_s_thesis/Hadoop_MapReduce_C___Demo/bin/
-INPUT_HOME=/home/kavolorn/master_s_thesis/Hadoop_MapReduce_C___Demo/
+INPUT_HOME=/home/kavolorn/master_s_thesis/Hadoop_MapReduce_C___Demo/input/
 STREAMIN_CLASS=$HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.3.0.jar
 JAVA_LIB_DIR=/usr/lib64/jvm/java/jre/lib/amd64/server
 
@@ -10,5 +10,5 @@ export LD_LIBRARY_PATH=$JAVA_LIB_DIR:$HADOOP_HOME/lib/native/
 
 # Run pipe
 
-cat input/input.typb | bin/mapper | bin/reducer > output
+cat ${INPUT_HOME}/input.typb | ${BINARY_HOME}/mapper | ${BINARY_HOME}/reducer > output
 cat output
